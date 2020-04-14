39cd6ab5a287b6bb83fde32a817cc873e7f66396
Florian Glorioz <florian.glorioz@gmail.com>
2020-04-14 02:12:32 +0200

    Add game logic

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use rand::{
 };
 use std::fmt;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub enum Cell {
     Live,
     Dead,
@@ -31,11 +31,64 @@ type Board = Vec<Line>;
 pub fn generate_board() -> Board {
     let mut rng = rand::thread_rng();
     let (x, y) = termion::terminal_size().unwrap();
-    let mut board: Board = vec![];
-    for _ in 0..y {
-        let line: Line = (0..x).map(|_| rng.gen::<Cell>()).collect::<Line>();
-        board.push(line);
+
+    (0..y)
+        .map(|_| (0..x).map(|_| rng.gen::<Cell>()).collect::<Line>())
+        .collect()
+}
+
+fn neighbor_count(board: &Board, (x, y): (usize, usize)) -> usize {
+    let neighbors = [
+        // top
+        (x.checked_sub(1), y.checked_sub(1)),
+        (Some(x), y.checked_sub(1)),
+        (x.checked_add(1), y.checked_sub(1)),
+        // sides
+        (x.checked_sub(1), Some(y)),
+        (x.checked_add(1), Some(y)),
+        // bottom
+        (x.checked_sub(1), y.checked_add(1)),
+        (Some(x), y.checked_add(1)),
+        (x.checked_add(1), y.checked_add(1)),
+    ];
+
+    neighbors
+        .iter()
+        .filter(|(x, y)| x.is_some() && y.is_some())
+        .map(|(x, y)| (x.unwrap(), y.unwrap()))
+        .filter(|(x, y)| board.get(*y).is_some() && board[*y].get(*x).is_some())
+        .map(|(x, y)| board[y][x])
+        .fold(
+            0,
+            |acc, cell| if let Cell::Live = cell { acc + 1 } else { acc },
+        )
+}
+
+fn change_cell_state(board: &Board, (x, y): (usize, usize)) -> Cell {
+    let count = neighbor_count(board, (x, y));
+
+    match board[y][x] {
+        Cell::Live => match count {
+            2..=3 => Cell::Live,
+            _ => Cell::Dead,
+        },
+        _ => match count {
+            3 => Cell::Live,
+            _ => Cell::Dead,
+        },
+    }
+}
+
+fn gen_2d_range(x: usize, y: usize) -> impl Iterator<Item = (usize, usize)> {
+    (0..y).flat_map(move |a| (0..x).map(move |b| (b, a)))
+}
+
+pub fn next_gen(board: Board) -> Board {
+    let (x, y) = termion::terminal_size().unwrap();
+    let mut new_board = vec![vec![Cell::Dead; x as usize]; y as usize];
+    for (x, y) in gen_2d_range(x as usize, y as usize) {
+        new_board[y][x] = change_cell_state(&board, (x, y));
     }
 
-    board
+    new_board
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use game_of_life::generate_board;
+use game_of_life::{generate_board, next_gen};
 use std::io::Read;
 use std::io::{stdout, Write};
+use std::{thread, time};
 use termion::async_stdin;
-use termion::input::{MouseTerminal};
+use termion::input::MouseTerminal;
 use termion::raw::IntoRawMode;
 
 const QUIT_KEY: u8 = b'q';
+const SLEEP_DURATION: u64 = 70;
 
 fn main() {
     let mut stdin = async_stdin().bytes();
     let mut stdout = MouseTerminal::from(stdout().into_raw_mode().unwrap());
-
-    let board = generate_board();
+    let duration = time::Duration::from_millis(SLEEP_DURATION);
+    let mut board = generate_board();
 
     loop {
         let event = stdin.next();
@@ -19,10 +21,14 @@ fn main() {
             break;
         }
 
-        for (index, line) in board.iter().enumerate() {
+        for (index, line) in board.clone().iter().enumerate() {
             let line = line.iter().map(|x| x.to_string()).collect::<String>();
             write!(stdout, "{}{}", termion::cursor::Goto(1, index as u16), line).unwrap();
         }
         stdout.flush().unwrap();
+
+        thread::sleep(duration);
+
+        board = next_gen(board);
     }
 }
